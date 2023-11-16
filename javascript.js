ef4dc46854dbe62da356e209871db9caafd5e67e
Kyle Schmidt <schmidtkyle021@gmail.com>
2023-11-16 13:26:30 -0500

    Create displayResults and displayStandings
    Shows a live feed of the results of a round, the standings of the game via DOM manipulation

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
 // call game function
 // game();
 
+//initialize win counter globally
+let pWins = 0;
+let cWins = 0;
+
 const playerOptions = document.querySelector("#player-options");
 const submitBtn = document.querySelector("#submit-button");
 
@@ -21,60 +25,6 @@ playerOptions.addEventListener('click', (event) => {
     }
 });
 
-submitBtn.addEventListener('click', displayRoundMessage);
-
-// // store their result
-// function getPlayerChoice() {
-//     playerChoice = prompt("Rock, Paper, or Scissors?");
-// // Returns null value if player clicks cancel, which later 
-// // allows the game to fully abort
-//     if (playerChoice === null){
-//         return null;
-//     }
-// // sets up playerChoice to be evaluated in all lowercase
-//     playerChoice = playerChoice.toLowerCase();
-//     while (playerChoice != "rock" 
-//         && playerChoice != "scissors" 
-//         && playerChoice != "paper" 
-//         && playerChoice != "r" 
-//         && playerChoice != "p" 
-//         && playerChoice != "s") {
-//             playerChoice = prompt(`Please enter "Rock" "Paper" or "Scissors"`);
-//         }
-// // formats the playerChoice string to Correct Case and returns
-//     if (playerChoice == "rock"
-//         || playerChoice == "paper"
-//         || playerChoice == "scissors") {
-//             firstLetter = playerChoice.charAt(0);
-//             firstLetter = firstLetter.toUpperCase();
-//             notFirstLetter = playerChoice.slice(1);
-//             playerChoice = (firstLetter + notFirstLetter);
-//             return playerChoice;
-//     // allows for shorthand for testing and the impatient
-//         } else if (playerChoice == "r"){
-//             playerChoice = "rock"
-//             firstLetter = playerChoice.charAt(0);
-//             firstLetter = firstLetter.toUpperCase();
-//             notFirstLetter = playerChoice.slice(1);
-//             playerChoice = (firstLetter + notFirstLetter);
-//             return playerChoice;
-//         } else if (playerChoice == "p"){
-//             playerChoice = "paper"
-//             firstLetter = playerChoice.charAt(0);
-//             firstLetter = firstLetter.toUpperCase();
-//             notFirstLetter = playerChoice.slice(1);
-//             playerChoice = (firstLetter + notFirstLetter);
-//             return playerChoice;
-//         } else if (playerChoice == "s"){
-//             playerChoice = "scissors"
-//             firstLetter = playerChoice.charAt(0);
-//             firstLetter = firstLetter.toUpperCase();
-//             notFirstLetter = playerChoice.slice(1);
-//             playerChoice = (firstLetter + notFirstLetter);
-//             return playerChoice;
-//         }
-// }
-
 // have computer randomly select "rock" "paper" or "scissors"
 function getComputerChoice() {
     computerChoice =  Math.floor(Math.random() * 3);
@@ -90,6 +40,8 @@ function getComputerChoice() {
     }
     return computerChoice;
 }
+
+submitBtn.addEventListener('click', game);
 
 //compare results
 function playRound(playerSelection, computerSelection) {
@@ -138,46 +90,42 @@ function displayRoundMessage() {
     computerSelection = getComputerChoice();
     resultsLi.textContent = playRound(playerSelection, computerSelection);
     resultsOl.appendChild(resultsLi);
+    return playRound(playerSelection, computerSelection);
+}
+
+function displayStandings() {
+    const standingsDiv = document.querySelector("#standings");
+    const standingsPara = document.querySelector('#standings-text');
+    standingsPara.textContent = "";
+    if (pWins > cWins) {
+        standingsPara.textContent = (` You are winning ${pWins}-${cWins}.`)
+    } else if (pWins === cWins) {
+        standingsPara.textContent = (` You are tied ${pWins}-${cWins}.`)
+    } else {
+        standingsPara.textContent = (` You are losing ${pWins}-${cWins}.`)
+    }
+    standingsDiv.appendChild(standingsPara);
 }
 
 //plays the best of 5 game
 function game() {
-// game will stop after either the user or computer wins 3 rounds (best of 5)
-    let pWins = 0;
-    let cWins = 0;
-    while (pWins < 3 && cWins < 3) {
-    // initialize player and computer selection variables 
-    // and call their functions
-        computerSelection = getComputerChoice();
-    // create variable for results
-        results = playRound(playerSelection, computerSelection)
-    // count wins for computer and player
-        if (results.includes("Win")) {
-            pWins++
-        } else if (results.includes("Lose")) {
-            cWins++
-        }
-    // add to results string with standings b/w player and computer
-        if (pWins > cWins) {
-            results = (results + ` You are winning ${pWins}-${cWins}.`)
-        } else if (pWins === cWins) {
-            results = (results + ` You are tied ${pWins}-${cWins}.`)
-        } else {
-            results = (results + ` You are losing ${pWins}-${cWins}.`)
-        }
-    // log and alert user of results
-        console.log(results)
-        console.log(computerSelection)
-        alert(results);
-            }
+// call function which will run the round and display the results to the page    
+    roundMessage = displayRoundMessage();
+// count wins for computer and player
+    if (roundMessage.includes("Win")) {
+        pWins++;
+    } else if (roundMessage.includes("Lose")) {
+        cWins++;
+    }
+    displayStandings();
 // alert the results of game
-    if (pWins == 3) {
+    if (pWins == 5) {
         alert(`Congratulations! You win!`)
         return
-    } else if (cWins == 3) {
+    } else if (cWins == 5) {
         alert(`You lose. Score one for the machines.`)
         return
     } else {
         console.log("error in game()")
     }
-        }
+}
